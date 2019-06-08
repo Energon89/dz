bd579007cec30ec90dcdd2dfa554b8b4cd5154d3
Energon <podymov89@gmail.com>
2019-06-08 18:19:37 +0300

    update dz#10.1

--- 10/10.1/src/modules/main.js
+++ 10/10.1/src/modules/main.js
@@ -1,19 +1,19 @@
-import { Clock } from './clock.js';
-import { Tabs } from './tabs.js';
-import { Stopwatch } from './stopwatch.js';
-import { Timer } from './timer.js';
+import { Clock } from "./clock.js";
+import { Tabs } from "./tabs.js";
+import { Stopwatch } from "./stopwatch.js";
+import { Timer } from "./timer.js";
 
 const clock = new Clock();
 const tabs = new Tabs();
 const stopwatch = new Stopwatch();
-const timer = new Timer;
+const timer = new Timer();
 
 function init() {
   clock.init();
   tabs.init();
   stopwatch.init();
   timer.init();
-  tabs.init('clock');
+  tabs.init("clock");
 }
 
 init();
--- 10/10.1/src/modules/stopwatch.js
+++ 10/10.1/src/modules/stopwatch.js
@@ -1,70 +1,73 @@
-const htmlElements = { 
-    output: document.querySelector('.container [data-mode="stopwatch"] .output'),
-    buttons: document.querySelectorAll('.container [data-mode="stopwatch"] .buttons button')
+const htmlElements = {
+  output: document.querySelector('.container [data-mode="stopwatch"] .output'),
+  buttons: document.querySelectorAll(
+    '.container [data-mode="stopwatch"] .buttons button'
+  )
 };
 
 let startTime = 0;
 let time, hours, minutes, seconds;
 let isRunning = false;
 
+function Stopwatch() {}
 
-function Stopwatch() {};
-  
 Stopwatch.prototype.init = function() {
-    htmlElements.buttons.forEach(function(button) {
-        button.addEventListener('click', function() { buttonClicked(this.dataset.mode);});
+  htmlElements.buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      buttonClicked(this.dataset.mode);
     });
-  };
-
-  function buttonClicked (mode){
-    switch (mode) {
-        case 'start':
-        if(!isRunning){
-        isRunning = true;
-        startTimer(startTime);}
-        break;
-        case 'stop':
-        clearInterval(time);
-        isRunning = false; 
-        break;
-        case 'reset':
-        resetTimer();
-        htmlElements.output.innerText = '00:00:00';
-        break;
-    }
+  });
 };
 
+function buttonClicked(mode) {
+  switch (mode) {
+    case "start":
+      if (!isRunning) {
+        isRunning = true;
+        startTimer(startTime);
+      }
+      break;
+    case "stop":
+      clearInterval(time);
+      isRunning = false;
+      break;
+    case "reset":
+      resetTimer();
+      htmlElements.output.innerText = "00:00:00";
+      break;
+  }
+}
+
 function startTimer(duration) {
+  const start = new Date().getTime();
 
-    const start = new Date().getTime();
+  time = setInterval(function() {
+    const difference = duration + (new Date().getTime() - start) / 1000;
 
-    time = setInterval( function() {
-    const difference = duration + ((new Date().getTime() - start) / 1000);
-    
     seconds = parseInt(difference % 60);
     minutes = parseInt((difference / 60) % 60);
-    hours = parseInt(difference/3600);
+    hours = parseInt(difference / 3600);
 
-    startTime = hours*3600 + minutes*60 + seconds;
+    startTime = hours * 3600 + minutes * 60 + seconds;
 
     if (hours < 10) {
-        hours = `0${hours}`;
+      hours = `0${hours}`;
     }
     if (minutes < 10) {
-        minutes = `0${minutes}`;
+      minutes = `0${minutes}`;
     }
     if (seconds < 10) {
-        seconds = `0${seconds}`;
+      seconds = `0${seconds}`;
     }
-    
+
     htmlElements.output.innerText = `${hours}:${minutes}:${seconds}`;
-    }, 1000);
+  }, 1000);
 }
 
 function resetTimer() {
-    clearInterval(time);
-    isRunning = false;
-    startTime = 0;
+  clearInterval(time);
+  isRunning = false;
+  startTime = 0;
 }
 
-  export { Stopwatch };
+export { Stopwatch };
